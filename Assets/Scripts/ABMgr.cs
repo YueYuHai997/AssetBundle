788a3f85711f09208e2464c9638d9a0b6e74d876
VTloser <39976734+VTloser@users.noreply.github.com>
2023-04-25 17:30:08 +0800

    AssetBunlde网络下载，配置文件生成
    AssetBunlde网络下载，配置文件生成，配置文件包含文件名，字节数，MD5
    
    ToDo：1.配置文件版本号 2.下载进度及下载失败重新下载 及 短点续下 3.配置文件md5对比

--- Assets/Scripts/ABMgr.cs
+++ Assets/Scripts/ABMgr.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Events;
-using UnityEngine.SceneManagement;
+using UnityEngine.Networking;
 
 
 public class ABMgr : MonoBehaviour
@@ -30,10 +35,29 @@ public class ABMgr : MonoBehaviour
         get
         {
 #if UNITY_EDITOR || UNITY_STANDALONE
-            return Application.streamingAssetsPath + "/";
+            return Application.streamingAssetsPath + "/AssetBundle/";
 #endif
         }
     }
+
+
+    /// <summary>    文件下载路径     </summary>
+    private string LoadSavePath
+    {
+        get
+        {
+#if UNITY_EDITOR || UNITY_STANDALONE
+            return Application.streamingAssetsPath + "/AssetBundle/";
+#endif
+        }
+    }
+
+    private string ServerPath = "http://192.168.10.104:5000/AssetBundle/";
+
+    private string CompareFile = "ABCompareInfo.txt";
+
+
+    #region 加载AssetBundle
 
     private void Awake()
     {
@@ -71,12 +95,15 @@ public class ABMgr : MonoBehaviour
         //});
 
 
-        for (int i = 0; i < 10; i++)
-        {
-            Instantiate(LoadRes<GameObject>("zombie1", "zombie1"));
-        }
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    Instantiate(LoadRes<GameObject>("zombie1", "zombie1"));
+        //}
 
         //LocalLoad();
+
+
+        NetLoadAsset();
     }
 
     AssetBundleManifest abmf;
@@ -220,7 +247,7 @@ public class ABMgr : MonoBehaviour
     }
 
     bool isAnsycLoadMainPage;
-    IEnumerator Ie_LoadABPage(string abName,UnityAction callback)
+    IEnumerator Ie_LoadABPage(string abName, UnityAction callback)
     {
         //获取主包
         if ((!abDic.ContainsKey(ABMainName) || abDic[ABMainName] == null) && !isAnsycLoadMainPage)
@@ -367,6 +394,10 @@ public class ABMgr : MonoBehaviour
     }
 
     #endregion
+    #endregion
+
+
+    #region 卸载AssetBundle
 
     /// <summary>
     /// 单个包卸载
@@ -392,6 +423,8 @@ public class ABMgr : MonoBehaviour
         abDic.Clear();
     }
 
+    #endregion
+
     /// <summary>
     /// 本地加载，不需要发布就可以加载。方便测试
     /// </summary>
@@ -403,5 +436,250 @@ public class ABMgr : MonoBehaviour
         Instantiate(gob);
 #endif 
     }
+
+    #region 下载AssetBundle
+
+
+    /// <summary>
+    /// 从网络下载AssetBundle
+    /// </summary>
+    /// <param name="path"></param>
+    /// <param name="filename"></param>
+    /// <param name="callBack"></param>
+    public void NetLoadAsset()
+    {
+
+        remoteABInfo.Clear();
+        localABInfo.Clear();
+        NeedLoadAB.Clear();
+
+        ReadLoaclAsset();
+
+
+        List<Task> task = new List<Task>();
+        //下载比较文件
+        StartCoroutine(LoadAssetBundle(ServerPath, CompareFile, () =>
+        {
+            //TODO-如果远端的配置文件和本地的配置文件MD5码相同则认为没有任何更新
+
+
+            //根据比较文件下载Ab文件
+            string info = File.ReadAllText(LoadSavePath + CompareFile);
+            string[] abs = info.Split("\n");
+            string[] infos = null;
+            foreach (var item in abs)
+            {
+                infos = item.Split("||");
+                //远端AB包信息
+                remoteABInfo.Add(infos[0], new ABInfo(infos[0], infos[1], infos[2]));
+                Debug.Log(infos[0]);
+            }
+            Debug.Log("配置文件下载完成");
+
+
+            foreach (var item in remoteABInfo.Keys)//根据远端AB文件作为参考
+            {
+                if (!localABInfo.ContainsKey(item) || localABInfo[item] != remoteABInfo[item]) //本地没有文件或者 本地文件与服务器不同
+                {
+                    NeedLoadAB.Add(item);
+                }
+                else if (localABInfo[item] == remoteABInfo[item]) //资源相同
+                {
+                    localABInfo.Remove(item);
+                }
+                else
+                {
+                    //  localABInfo 剩下的都是本地多余信息 选择删除
+                }
+            }
+
+            if (NeedLoadAB.Count == 0)
+            {
+                Debug.Log("当前资源为最新版本无需下载");
+            }
+            else
+            {
+                for (int i = 0; i < NeedLoadAB.Count; i++)
+                {
+                    string loadname = NeedLoadAB[i];
+                    Debug.Log($"需要下载的包名{loadname}，下载大小为{ remoteABInfo[loadname].size / 1024f / 1024f} MB");
+                    StartCoroutine(LoadAssetBundle(ServerPath, loadname, () =>
+                    {
+                        Debug.Log("Load Success");
+                        NeedLoadAB.Remove(loadname);//下载成功之后从NeedLoadAB 移除
+                    }));
+                }
+            }
+            //ToDo 文件下载失败 再从NeedLoadAB下载没下载完成的部分 设置最大下载次数
+
+
+        }));
+
+        //LoadIISCompareFile(path, filename, callBack);
+
+    }
+
+    private void ReadLoaclAsset()
+    {
+        DirectoryInfo directory = Directory.CreateDirectory(LoadSavePath);
+        //该目录下的所有文件信息
+        FileInfo[] fileInfos = directory.GetFiles();
+        foreach (var item in fileInfos)
+        {
+            if (item.Extension == "") //ab包没有后缀
+            {
+                localABInfo.Add(item.Name, new ABInfo(item.Name, item.Length, GetMD5(item.FullName)));
+            }
+        }
+    }
+
+    private static string GetMD5(string filepath)
+    {
+        //根据文件路径，获取文件流信息
+        //将文件以流形式打开
+        using (FileStream file = new FileStream(filepath, FileMode.Open))
+        {
+            //声明一个MD5对象 生成MD5码
+            MD5 mD5 = new MD5CryptoServiceProvider();
+            var md5Info = mD5.ComputeHash(file);
+
+            //关闭文件流
+            file.Close();
+
+            //把16字节转换成16进制拼接字符串，减少MD5码长度
+            StringBuilder sb = new StringBuilder();
+            for (var i = 0; i < md5Info.Length; i++)
+            {
+                sb.Append(md5Info[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+    }
+
+
+    /// <summary>  远端AB包   </summary>
+    private Dictionary<string, ABInfo> remoteABInfo = new Dictionary<string, ABInfo>();
+
+    /// <summary>  本地AB包   </summary>
+    private Dictionary<string, ABInfo> localABInfo = new Dictionary<string, ABInfo>();
+
+    // <summary>  需要下载的AB包   </summary>
+    private List<string> NeedLoadAB = new List<string>();
+
+
+    private class ABInfo
+    {
+        public string name;
+        public long size;
+        public string md5;
+
+        public ABInfo(string name, string size, string md5)
+        {
+            this.name = name;
+            this.size = long.Parse(size);
+            this.md5 = md5;
+        }
+        public ABInfo(string name, long size, string md5)
+        {
+            this.name = name;
+            this.size = size;
+            this.md5 = md5;
+        }
+
+        public static bool operator ==(ABInfo a, ABInfo b)
+        {
+            if (!(a is null) && !(b is null) && a.md5 == b.md5) return true;
+            return false;
+        }
+
+        public static bool operator !=(ABInfo a, ABInfo b)
+        {
+            if (!(a is null) && !(b is null) && a.md5 == b.md5) return false;
+            return true;
+        }
+
+    }
+
+    /// <summary>
+    /// 从IIS服务器下载对比文件
+    /// </summary>
+    public void LoadIISCompareFile(string serverPath, string filenName, UnityAction callBack = null)
+    {
+        WebClient _webClient = new WebClient();
+        //使用默认的凭据——读取的时候，只需默认凭据就可以
+        _webClient.Credentials = CredentialCache.DefaultCredentials;
+        string uri = serverPath + filenName;
+        //下载的链接地址（文件服务器）
+        System.Uri _uri = new System.Uri(uri);
+        //注册下载进度事件通知
+        //_webClient.DownloadProgressChanged += _webClient_DownloadProgressChanged;
+        //注册下载完成事件通知
+        _webClient.DownloadFileCompleted += (_, Y) =>
+        {
+            Debug.Log($"{filenName}||{Y.Error}");
+            AssetDatabase.Refresh();
+            callBack?.Invoke();
+        };
+        //异步下载
+        _webClient.DownloadFileAsync(_uri, LoadSavePath + filenName);
+    }
+
+    /// <summary>
+    /// 使用webRequest下载
+    /// </summary>
+    /// <param name="Serverpath"></param>
+    /// <param name="filename"></param>
+    /// <param name="CallBack"></param>
+    /// <returns></returns>
+    IEnumerator LoadAssetBundle(string serverPath, string fileName, UnityAction callBack = null)
+    {
+        //服务器上的文件路径
+        string uri = serverPath + fileName;
+
+        using (var webRequest = UnityWebRequest.Get(uri))
+        {
+            yield return webRequest.SendWebRequest();
+
+            if (webRequest.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.Log("Download Error:" + webRequest.error);
+            }
+            else
+            {
+                //下载完成后执行的回调
+                if (webRequest.isDone)
+                {
+                    byte[] results = webRequest.downloadHandler.data;
+                    if (!Directory.Exists(LoadSavePath))
+                    {
+                        Directory.CreateDirectory(LoadSavePath);
+                    }
+                    using (FileStream fs = File.Create(LoadSavePath + fileName))
+                    {
+                        fs.Write(results, 0, results.Length);
+                        fs.Close();
+                    }
+
+                    //using (FileStream fs = File.Create(fileName))  //Task线程不能访问applocation
+                    //{
+                    //    fs.Write(results, 0, results.Length);
+                    //    fs.Close();
+                    //}
+
+                    //FileInfo fileInfo = new FileInfo(LoadSavePath + filename);
+                    //FileStream fs = fileInfo.Create();
+                    ////fs.Write(字节数组, 开始位置, 数据长度);
+                    //fs.Write(results, 0, results.Length);
+                    //fs.Flush(); //文件写入存储到硬盘
+                    //fs.Close(); //关闭文件流对象
+                    //fs.Dispose(); //销毁文件对象
+
+                    callBack?.Invoke();
+                }
+            }
+        }
+    }
+
+    #endregion
 
 }
---
+++ Assets/Scripts/Editor/ABEditor.cs
@@ -0,0 +1,182 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using System.Security.Cryptography;
+using System.Text;
+using System.Net;
+using System;
+using System.Threading.Tasks;
+
+public class ABEditor
+{
+    public static string path = Application.streamingAssetsPath + "/AssetBundle/";
+
+
+    [MenuItem("AB包工具/创建对比文件")]
+    public static void CreateABCompareFile()
+    {
+        //文件夹信息类
+        DirectoryInfo directory = Directory.CreateDirectory(path);
+        //该目录下的所有文件信息
+        FileInfo[] fileInfos = directory.GetFiles();
+
+        //将ab的文件名、大小、MD5码存入资源对比文件
+        string abCompareInfo = null;
+        foreach (var item in fileInfos)
+        {
+            if (item.Extension == "") //ab包没有后缀
+            {
+                abCompareInfo += item.Name + "||" + item.Length + "||" + GetMD5(item.FullName);
+                abCompareInfo += "\n";
+            }
+        }
+        abCompareInfo = abCompareInfo.TrimEnd('\n');
+        Debug.Log(abCompareInfo.ToString());
+        //存储AB包对比文件
+        File.WriteAllText(path + "ABCompareInfo.txt", abCompareInfo.ToString());
+        //编辑器刷新
+        AssetDatabase.Refresh();
+        Debug.Log("AB包对比文件生成成功");
+    }
+
+    [MenuItem("AB包工具/上传AB文件和对比文件")]
+    public static void UpLoadALLAB()
+    {
+        //文件夹信息类
+        DirectoryInfo directory = Directory.CreateDirectory(path);
+        //该目录下的所有文件信息
+        FileInfo[] fileInfos = directory.GetFiles();
+
+        foreach (var item in fileInfos)
+        {
+            if (item.Extension == ".txt" ||
+                item.Extension == ".txt") //ab包没有后缀
+            {
+                //上传
+                UpLoad(item.FullName, item.Name);
+            }
+        }
+    }
+
+    private static void UpLoad(string filePath, string fileName)
+    {
+        //FTPUpLoad(filePath, fileName);
+        IISIpLoad(filePath, fileName);
+    }
+
+    /// <summary>
+    /// FTP上传
+    /// </summary>
+    /// <param name="filePath"></param>
+    /// <param name="fileName"></param>
+    private async static void FTPUpLoad(string filePath, string fileName)
+    {
+        await Task.Run(() =>
+        {
+            try
+            {
+                //1.创建FTP链接 用于上传
+                FtpWebRequest req = FtpWebRequest.Create(new Uri("ftp://127.0.0.1/AB/PC" + fileName)) as FtpWebRequest;
+                //2. 设置一个通信凭证  这样才能上传
+                NetworkCredential nc = new NetworkCredential("usetname", "password");
+                req.Credentials = nc;
+                //3.其他设置
+                //设置代理为空
+                req.Proxy = null;
+                //请求完毕后是否关闭控制连接
+                req.KeepAlive = false;
+                //命令-上传
+                req.Method = WebRequestMethods.Ftp.UploadFile;
+                //指定传输类型 2禁止
+                req.UseBinary = true;
+                //4.上传文件
+                //ftp流文件
+                Stream uploadSteam = req.GetRequestStream();
+                //读取文件信息 写入流对象
+                using (FileStream file = File.OpenRead(filePath))
+                {
+                    //一点一点的上传内容
+                    byte[] bytes = new byte[2048];
+                    //返回值代表读取了多少个字节
+                    int contentLength = file.Read(bytes, 0, bytes.Length);
+                    //循环上传
+                    while (contentLength != 0)
+                    {
+                        //写入到上传流中
+                        uploadSteam.Write(bytes, 0, contentLength);
+                        //写完再读
+                        contentLength = file.Read(bytes, 0, bytes.Length);
+                    }
+
+                    //循环完毕后 上传结束
+                    file.Close();
+                    uploadSteam.Close();
+                }
+                Debug.Log($"{fileName}上传成功");
+            }
+            catch (Exception e)
+            {
+                Debug.Log($"{fileName}上传失败：[{ e }]");
+            }
+        });
+    }
+
+
+    private static void IISIpLoad(string filePath, string fileName)
+    {
+        Debug.Log(fileName);
+        try
+        {
+            //定义_webClient对象
+            WebClient _webClient = new WebClient();
+            //使用Windows登录方式
+            _webClient.Credentials = new NetworkCredential("IUSR", "");
+            //上传的链接地址（文件服务器）
+            Uri _uri = new Uri(@$"http://192.168.10.104:5000/AssetBundle/");
+            //上传成功事件注册
+            _webClient.UploadFileCompleted += (_, Y) => 
+            {
+                Debug.Log($"{fileName}||{Y.Result}");
+            };
+            //异步从D盘上传文件到服务器
+            _webClient.UploadFileAsync(_uri, "PUT", filePath);
+            Console.ReadKey();
+        }
+        catch (Exception e)
+        {
+            Debug.Log($"{fileName}上传失败：[{ e }]");
+        }
+
+    }
+
+
+    /// <summary>
+    /// 获取文件MD5码
+    /// </summary>
+    /// <param name="filepath"></param>
+    /// <returns></returns>
+    private static string GetMD5(string filepath)
+    {
+        //根据文件路径，获取文件流信息
+        //将文件以流形式打开
+        using (FileStream file = new FileStream(filepath, FileMode.Open))
+        {
+            //声明一个MD5对象 生成MD5码
+            MD5 mD5 = new MD5CryptoServiceProvider();
+            var md5Info = mD5.ComputeHash(file);
+
+            //关闭文件流
+            file.Close();
+
+            //把16字节转换成16进制拼接字符串，减少MD5码长度
+            StringBuilder sb = new StringBuilder();
+            for (var i = 0; i < md5Info.Length; i++)
+            {
+                sb.Append(md5Info[i].ToString("x2"));
+            }
+            return sb.ToString();
+        }
+    }
+}
